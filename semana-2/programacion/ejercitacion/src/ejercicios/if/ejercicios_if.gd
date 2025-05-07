5891075661fa66e8b5ac3d6eb343add3ac7a7b4f
Moortin35 <moortindelgado@gmail.com>
2025-05-07 17:46:42 -0300

    Completado la ejercitación de if

--- semana-2/programacion/ejercitacion/src/ejercicios/if/ejercicios_if.gd
+++ semana-2/programacion/ejercitacion/src/ejercicios/if/ejercicios_if.gd
@@ -5,11 +5,24 @@ func _ready() -> void:
 	#El error va a desaparecer una vez que ingresemos codigo en la funcion _ready()
 	var persona1 = {nombre = "Carlos", edad = 10}
 	var persona2 = {nombre = "Roberto", edad = 20}
-	edad(persona1, persona2)
 	var numerillo = 3
+	var caracter = "a"
+	var a = 7
+	var b = 2
+	var c = 5
+	var d = 1
+	var e = 25
+	
+	edad(persona1, persona2)
 	par_o_impar(numerillo)
 	es_pos_neg(numerillo)
 	multiplo_tres(numerillo)
+	minus_o_mayus(caracter)
+	mayor_de_tres(a,b,c)
+	el_mayor_de_cinco(a,b,c,d,e)
+	mas_cercano(d,e)
+	expresion("(El arsenal de inglaterra cayó 2 a 0 en parís)")
+	raiz_de_numero(16)
 	
 ##Ejercicio 1: Edad de 2 Personas
 #compara la edad de dos personas y muestra quién es mayor o si tienen la misma edad.
@@ -49,25 +62,51 @@ func multiplo_tres(numero):
 
 ##Ejercicio 5: Mayúsculas y Minúsculas
 #Dado un carácter, indicar si es mayúscula o minúscula.
-func min_or_mayus(caracter):
-	print("")
-
+func minus_o_mayus(caracter):
+	if caracter == caracter.to_lower():
+		print("Es miniscula")
+	elif caracter == caracter.to_upper():
+		print("Es mayuscula")
+	else:
+		print("No es ninguno de los 2")
 
 ##Ejercicio 6: Mayor de 3 Números
 #Dado tres números, mostrar cuál es el mayor.
-
-
+func mayor_de_tres(a,b,c):
+	if a == b and b == c:
+		print ("Los tres números son iguales")
+	elif max(a,b,c) == b:
+		print ("El mayor de los tres es: ", b)
+	elif max(a,b,c) == c:
+		print ("El mayor de los tres es: ", c)
+	else:
+		print ("El mayor de los tres es: ", a)
+		
 ##Ejercicio 7: Mayor de 5 Números
 #Determinar cuál es el mayor entre cinco números distintos.
-
+func el_mayor_de_cinco(a,b,c,d,e):
+	var res = max(a,b,c,d,e)
+	print("El mayor de los cinco es: ", res)
 
 ##Ejercicio 8: Número más Cercano
 #Determinar qué número está más cerca de 10: a o b.
-
+func mas_cercano(a,b):
+	if abs(a-10) < abs(b-10):
+		print(a, " es más cercano a 10")
+	else:
+		print(b, " es más cercano a 10")
 
 ##Ejercicio 9: Paréntesis
 #Verificar si una expresión (como texto) comienza y termina con paréntesis.
-
-
+func expresion(palabra):
+	if (palabra[0] == "(") and (palabra[-1] == ")"):
+		print("la expresión esta contenida entre paréntesis")
+	else:
+		print("La expresión no comienza y termina con paréntesis")
 ##Ejercicio 10: Raíz cuadrada de un número
 #Pedir un número. Si es positivo, imprimir su raíz cuadrada.
+func raiz_de_numero(numerito):
+	if(numerito > 0):
+		print("la raíz de ", numerito, " es: ", sqrt(numerito))
+	else:
+		print("valor invalido para calcular raiz")
